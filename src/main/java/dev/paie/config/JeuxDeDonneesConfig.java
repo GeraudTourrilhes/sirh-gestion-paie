826c23c8da12433a9ae1b4ea5ba6b57e7ccf5f5a
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-05-25 15:52:33 +0200

    tp jpa

--- src/main/java/dev/paie/config/JeuxDeDonneesConfig.java
+++ src/main/java/dev/paie/config/JeuxDeDonneesConfig.java
@@ -1,10 +1,10 @@
 package dev.paie.config;
 
-import org.springframework.context.annotation.*;
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ImportResource("classpath:jdd-config.xml")
 public class JeuxDeDonneesConfig {
-	
+
 }
