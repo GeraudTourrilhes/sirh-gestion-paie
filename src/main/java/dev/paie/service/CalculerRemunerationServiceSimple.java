9015fd8fb1aef42e644f550393fa5c05c61271e7
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-05-29 14:58:37 +0200

    h2Config

--- src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
+++ src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.paie.entite.BulletinSalaire;
 import dev.paie.entite.Cotisation;
@@ -11,6 +12,7 @@ import dev.paie.entite.ResultatCalculRemuneration;
 import dev.paie.util.PaieUtils;
 
 @Service
+@Transactional
 public class CalculerRemunerationServiceSimple implements CalculerRemunerationService {
 
 	@Autowired
